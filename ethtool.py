736bb0be1bf82a43f33d4209b1d774cefa0fe660
Markus Kont <markuskont@gmail.com>
2014-11-01 23:53:32 +0200

    Refactor

--- ethtool.py
+++ ethtool.py
@@ -8,19 +8,25 @@ except:
     print "no snmp-passpersist module installed:"
     print "pip install snmp-passpersist"
 
-def read_smart():
+def execute_command():
     cmd='ethtool -S wlan0'
     
     return os.popen(cmd).read()
 
-def main():
-    stats=read_smart()
+def update():
+
+    stats=execute_command()
 
     for line in stats.split("\n"):
         data = re.match('\s+(\w+): (\d+)', line)
         if not data: continue
         name = data.group(1)
         value = data.group(2)
+        #pp.add_int(oid, value)
+
+def main():
+    update()
+
 
 if __name__ == "__main__":
     main()
