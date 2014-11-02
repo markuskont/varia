6ac3ac729cccba96e04692ca3ecfb6705a88a73c
Markus Kont <markuskont@gmail.com>
2014-11-02 13:03:05 +0200

    pass-persist loop does not work - prolly because my generated OID is prefixed with a dot

--- ethtool.py
+++ ethtool.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/python -u
 
 import sys, os, re
 
@@ -24,18 +24,22 @@ def gen_oid(argv):
 def update():
 
     stats=execute_command()
+    # temp
+    index=1
 
     for line in stats.split("\n"):
         data = re.match('\s+(\w+): (\d+)', line)
         if not data: 
             continue
         name = data.group(1)
-        value = data.group(2)
-        oid = gen_oid(name)
-        #pp.add_cnt_64bit(oid, value)
+        value = int(data.group(2))
+        #oid = gen_oid(name)
+        # temp
+        oid = "%d.%s" % (index,
+                 ".".join([str(ord(a)) for a in name]))
+        pp.add_cnt_64bit(oid, value)
 
-def main():
-    update()
+update()
 
-if __name__ == "__main__":
-    main()
+pp = snmp.PassPersist('.1.3.6.1.4.1.39178.100.1.1.1.2')
+pp.start(update, 10)
