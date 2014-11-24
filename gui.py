35f86da12d4197d40686a459479c2da4420ce99a
markuskont <alias013@gmail.com>
2014-11-24 18:10:35 +0200

    Initial
    simple tkinter with copy-paste dev

---
+++ gui.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+# Simple GUI test with tkinter module
+
+import Tkinter
+
+def main():
+	top = Tkinter.Tk()
+	# Code to add widgets will go here...
+	top.mainloop()
+
+if __name__ == "__main__":
+	main()
