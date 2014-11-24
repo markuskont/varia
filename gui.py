273db2833af8eb59b16a6b0b11e6613b495020b0
markuskont <alias013@gmail.com>
2014-11-24 18:33:53 +0200

    Basic input

--- gui.py
+++ gui.py
@@ -4,10 +4,29 @@
 
 import Tkinter
 
-def main():
-	top = Tkinter.Tk()
-	# Code to add widgets will go here...
-	top.mainloop()
+def textinput():
+	print hi
+
+def main(argv):
+	window = Tkinter.Tk()
+	
+	# text input
+
+	Tkinter.Label(window, text="First").grid(row=0)
+	Tkinter.Label(window, text="Second").grid(row=1)
+
+	e1 = Tkinter.Entry(window)
+	e2 = Tkinter.Entry(window)
+
+ 	e1.grid(row=0, column=1)
+ 	e2.grid(row=1, column=1)
+
+	#label.grid()
+	#label.entry = Tkinter.Entry(label)
+	#label.entry.grid(column=0,row=0,sticky='EW')
+
+	window.mainloop()
+
 
 if __name__ == "__main__":
-	main()
+	main("asd")
