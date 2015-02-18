225c0c8db6631e931415a7adad374e24369ddb54
root <root@testruuter.spin.ee>
2015-02-18 14:28:55 +0200

    Ported my script to perl. Because why not.

---
+++ get_cpu_temp.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+my $cmd = "sensors";    
+my @output = `$cmd`;    
+chomp @output;
+
+foreach my $line (@output)
+{
+	if($line =~ m/^(Core) (\d):\s+[+-]?(\d+)\.\d+\s*C/){
+
+		print "$1$2:$3"
+
+	}
+
+}
