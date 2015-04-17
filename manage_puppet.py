99b1a39e4586e88683b01c120a81e9c0a655038a
Markus <markus@spin.ee>
2015-04-17 16:04:40 +0300

    Core functionality works

--- manage_puppet.py
+++ manage_puppet.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import sys, os, argparse, re, json, copy, subprocess
+import sys, os, argparse, re, json, copy, subprocess, hashlib, datetime
 from stat import *
 
 ######################################
@@ -20,61 +20,10 @@ def parse_arguments(environments):
 
 	parser = argparse.ArgumentParser()
 	parser.add_argument('-e', '--environment', choices=environments, default='production', help='Puppet environment')
-	parser.add_argument('-d', '--cadir', default='/tmp/', help='Absolute path to CA root directory')
+	parser.add_argument('-d', '--cadir', default='/tmp/CA/', help='Absolute path to CA root directory')
 	args = parser.parse_args()
 
 	return args
-
-# For intents and purposes, this is functionally main();
-# At least for now
-def extract_nodes_from_puppet_env():
-	
-	puppet_rootdir=PUPPET_ROOT_DIR + SELECTED_ENV + '/'
-	
-
-	pattern_puppet_site_manifest = re.compile('site\S+\.pp')
-	pattern_puppet_node_definition = re.compile("^node\s+'(\S+)'\s*\{")
-	pattern_hostname_from_fqdn = re.compile("(\S+)\.\S+\.\S+$")
-
-	files = list(recursive_file_gen(puppet_rootdir, pattern_puppet_site_manifest))
-
-	nodes = extract_data_from_file(files, pattern_puppet_node_definition)
-
-	ca_working_folders = [ 'config.d', 'requests', 'private', 'certificates']
-	ca_working_folders = [ CADIR + '/' + folder for folder in ca_working_folders]
-
-	# replace this part
-#	ca_config_rootdir=CADIR + '/' + 'config.d' 
-#	ca_req_rootdir=CADIR + '/' + 'requests' 
-#	ca_key_rootdir=CADIR + '/' + 'keys' 
-#	ca_cert_rootdir=CADIR + '/' + 'certificates' 
-	ca_config_rootdir=ca_working_folders[0]
-	ca_req_rootdir=ca_working_folders[1]
-	ca_key_rootdir=ca_working_folders[2]
-	ca_cert_rootdir=ca_working_folders[3]
-
-	check_dir_list(ca_working_folders)
-
-	for node_fqdn in nodes:
-
-		hostname = pattern_hostname_from_fqdn.match(node_fqdn).group(1)
-
-		config_file_abs_path = ca_config_rootdir + '/' + node_fqdn + '.conf'
-		#key_file_abs_path = CADIR + '/'
-		#req_file_abs_path = ca_config_rootdir + '/' + node_fqdn + '.conf'
-
-		config = open(config_file_abs_path, 'w')
-		config.write(OPENSSL_CONFIG_TEMPLATE % {'hostname': hostname})
-		config.close()
-
-		#openssl('req', '-new', '-key', dfile('key'), '-out', dfile('request'), '-config', config_file_abs_path)
-
-	# DEBUG OUTPUT
-	print "Environment: " + puppet_rootdir
-	print "CA root dir: " + CADIR
-	print "CA key file: " + CA_KEY
-	print "CA certificate file: " + CA_CERT
-	# END DEBUG OUTPUT
 
 def check_dir_list(args_list):
 
@@ -85,6 +34,24 @@ def check_dir_list(args_list):
 			os.mkdir(path)
 		if str(oct(os.stat(path)[ST_MODE])[-4:]) != str(folder_mask):
 			os.chmod(path, folder_mask)
+
+def validate_CA_files():
+
+	if not os.path.exists(CADIR):
+		print "CA filesystem root does not exist!"
+		print "Please consider creating with the following command:"
+		print "mkdir %s && chmod 700 %s" %(CADIR, CADIR)
+		sys.exit(2)
+	if not os.path.isfile(CA_KEY):
+		print "CA Key file does not exist!"
+		print "Please consider creating with the following command:"
+		print "openssl genrsa -out %s/cakey.pem 4096 && chmod 0600 %s/cakey.pem" %(CADIR,CADIR)
+		sys.exit(2)
+	if not os.path.isfile(CA_CERT):
+		print "CA Certificate file does not exist!"
+		print "Please consider creating with the following command:"
+		print "openssl req -new -x509 -days 3650 -key %s/cakey.pem -out %s/cacert.pem" % (CADIR,CADIR)
+		sys.exit(2)
 			
 		
 # GLOBAL VARIABLES
@@ -96,9 +63,10 @@ CADIR = os.path.abspath(ARGS.cadir)
 OPENSSL = '/usr/bin/openssl'
 KEY_SIZE = 1024
 DAYS = 3650
-# TODO: Validate presence of files!
 CA_CERT = CADIR + '/' + 'cacert.pem'
 CA_KEY = CADIR + '/' + 'cakey.pem'
+
+X509_EXTRA_ARGS = ()
 
 ############################
 # END SHARED LOGIC
@@ -125,6 +93,27 @@ def extract_data_from_file(files, pattern):
 
 	return results
 
+def extract_nodes_from_puppet_env():
+	
+	puppet_rootdir=PUPPET_ROOT_DIR + SELECTED_ENV + '/'
+	
+
+	pattern_puppet_site_manifest = re.compile('site\S+\.pp')
+	pattern_puppet_node_definition = re.compile("^node\s+'(\S+)'\s*\{")
+
+	files = list(recursive_file_gen(puppet_rootdir, pattern_puppet_site_manifest))
+
+	nodes = extract_data_from_file(files, pattern_puppet_node_definition)
+
+	# DEBUG OUTPUT
+	print "Environment: " + puppet_rootdir
+	print "CA root dir: " + CADIR
+	print "CA key file: " + CA_KEY
+	print "CA certificate file: " + CA_CERT
+	# END DEBUG OUTPUT
+
+	return nodes
+
 ##########################################
 # END NODE EXTRACTION PART
 # START OPENSSL PART
@@ -144,27 +133,58 @@ ST                     = Harjumaa
 L                      = Tallinn
 O                      = Spin TEK AS
 OU                     = Administration
-CN                     = %(hostname)s.spin.ee
+CN                     = %(fqdn)s.spin.ee
 emailAddress           = admin@spin.ee
 
 [ v3_req ]
 # Extensions to add to a certificate request
 basicConstraints = CA:FALSE
 keyUsage = nonRepudiation, digitalSignature, keyEncipherment
+
 subjectAltName = @alt_names
 
 [ alt_names ]
-DNS.1 = %(hostname)s.spin.sise
-DNS.2 = %(hostname)s.keskus.sise
+DNS.1 = %(fqdn)s
+DNS.2 = %(fqdn)s
 """
 
 def openssl(*args):
 	cmdline = [OPENSSL] + list(args)
 	subprocess.check_call(cmdline)
 
-def gencert(fqdn, rootdir=CADIR, keysize=KEY_SIZE, days=DAYS, ca_cert=CA_CERT, ca_key=CA_KEY):
-	pass
-	
+def gencert(nodes, rootdir=CADIR, keysize=KEY_SIZE, days=DAYS, ca_cert=CA_CERT, ca_key=CA_KEY):
+
+	pattern_hostname_from_fqdn = re.compile("(\S+)\.\S+\.\S+$")
+
+	ca_working_folders = [ 'config.d', 'requests', 'private', 'certificates']
+	ca_working_folders = [ CADIR + '/' + folder for folder in ca_working_folders]
+
+#	ca_config_rootdir=ca_working_folders[0]
+#	ca_req_rootdir=ca_working_folders[1]
+#	ca_key_rootdir=ca_working_folders[2]
+#	ca_cert_rootdir=ca_working_folders[3]
+
+	check_dir_list(ca_working_folders)
+
+	for node_fqdn in nodes:
+
+		hostname = pattern_hostname_from_fqdn.match(node_fqdn).group(1)
+
+		conf_file_abs_path = ca_working_folders[0] + '/' + node_fqdn + '.conf'
+		req_file_abs_path = ca_working_folders[1] + '/' + node_fqdn + '.csr'
+		key_file_abs_path = ca_working_folders[2] + '/' + node_fqdn + '.key'
+		cert_file_abs_path = ca_working_folders[3] + '/' + node_fqdn + '.cert'
+
+		config = open(conf_file_abs_path, 'w')
+		config.write(OPENSSL_CONFIG_TEMPLATE % {'fqdn': hostname})
+		config.close()
+
+		if not os.path.isfile(key_file_abs_path):
+			openssl('genrsa', '-out', key_file_abs_path, str(keysize))
+
+		if not os.path.isfile(cert_file_abs_path):
+			openssl('req', '-new', '-key', key_file_abs_path, '-out', req_file_abs_path, '-config', conf_file_abs_path)
+			openssl('x509', '-req', '-days', str(days), '-in', req_file_abs_path, '-CA', ca_cert, '-CAkey', ca_key, '-set_serial', '0x%s' % hashlib.md5(node_fqdn + str(datetime.datetime.now())).hexdigest(), '-out', cert_file_abs_path, '-extensions', 'v3_req', '-extfile', conf_file_abs_path, *X509_EXTRA_ARGS)
 
 ##################
 # END OPENSSL PART
@@ -172,7 +192,9 @@ def gencert(fqdn, rootdir=CADIR, keysize=KEY_SIZE, days=DAYS, ca_cert=CA_CERT, c
 
 def main():
 
-	extract_nodes_from_puppet_env()
+	validate_CA_files()
+	nodes = extract_nodes_from_puppet_env()
+	gencert(nodes)
 
 if __name__ == "__main__":
 
